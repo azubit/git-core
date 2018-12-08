683a4be3ecb6ef9f3fd103a1e173d8d95e6241df
Ankur Garg <ankur.garg@hsc.com>
2018-12-08 11:04:58 +0530

    First Commit for CoreBackend

---
+++ gtp/src/main/java/com/azubit/web/gtp/GtpApplication.java
@@ -0,0 +1,12 @@
+package com.azubit.web.gtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtpApplication.class, args);
+	}
+}
---
+++ gtp/src/test/java/com/azubit/web/gtp/GtpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.azubit.web.gtp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GtpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
